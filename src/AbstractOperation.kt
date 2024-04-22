d78dcb63f267864650fbbc4d09c9c231e7c0f8c9
임성우 <seongwoorang@naver.com>
2024-04-22 11:09:09 +0900

    Add Abstract Class

---
+++ src/AbstractOperation.kt
@@ -0,0 +1,7 @@
+abstract class AbstractOperation {
+    abstract fun add(a: Int, b: Int): Int
+    abstract fun subtract(a: Int, b: Int): Int
+    abstract fun multiple(a: Int, b: Int): Int
+    abstract fun divide(a: Int, b: Int): Int
+    abstract fun remainder(a: Int, b: Int): Int
+}
