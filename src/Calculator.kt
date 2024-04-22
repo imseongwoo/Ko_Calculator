a8040bb66cf28fb486ad3ad67520c73a96a5db1a
임성우 <seongwoorang@naver.com>
2024-04-22 11:09:36 +0900

    Add Calculator

--- src/Calculator.kt
+++ src/Calculator.kt
@@ -1,7 +1,33 @@
-class Calculator {
+class Calculator(private val operation: AbstractOperation) {
+    fun addOperation(a: Int, b: Int): Int {
+        return operation.add(a, b)
+    }
+
+    fun subtractOperation(a: Int, b: Int): Int {
+        return operation.subtract(a, b)
+    }
+
+    fun divideOperation(a: Int, b: Int): Int {
+        return operation.divide(a, b)
+    }
+
+    fun multipleOperation(a: Int, b: Int): Int {
+        return operation.multiple(a, b)
+    }
+
+    fun remainderOperation(a: Int, b: Int): Int {
+        return operation.remainder(a, b)
+    }
 
 }
 
 fun main() {
-    println("Hello World!!!")
+    val abstractOperationImpl = AbstractOperationImpl()
+    val calculator = Calculator(abstractOperationImpl)
+
+    println("1 더하기 2 결과는 : ${calculator.addOperation(1, 2)} 입니다")
+    println("1 빼기 2 결과는 : ${calculator.subtractOperation(1, 2)} 입니다")
+    println("4 나누기 2 결과는 : ${calculator.divideOperation(4, 2)} 입니다")
+    println("1 곱하기 2 결과는 : ${calculator.multipleOperation(1, 2)} 입니다")
+    println("1과 2의 나머지연산 결과는 : ${calculator.remainderOperation(1, 2)} 입니다")
 }
