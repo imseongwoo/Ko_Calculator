845ece721e6c1f9c77984b61d2544b5aa1d5444b
임성우 <seongwoorang@naver.com>
2024-04-23 11:27:58 +0900

    Add Exception

--- src/main.kt
+++ src/main.kt
@@ -6,11 +6,15 @@ fun main() {
         println("계산식을 입력해주세요 (엔터키를 누르면 프로그램이 종료됩니다.):")
         val input = readlnOrNull() ?: break
 
-        if (input.isBlank()) {
-            break
+        try {
+            if (input.isBlank()) {
+                break
+            }
+            val postfix = toPostfix(input)
+            val result = calculatePostfix(postfix)
+            println("결과값: $result")
+        } catch (e: Exception) {
+            println("지원되는 연산자를 사용해주세요. ('+','-','%','/','*')")
         }
-        val postfix = toPostfix(input)
-        val result = calculatePostfix(postfix)
-        println("결과값: $result")
     }
 }
